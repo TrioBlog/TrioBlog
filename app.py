6797ccbc6125dd0f4bf8cbeff3e2acaf08aec818
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-07 17:40:25 -0500

    app.py organization

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from models.db import db
 from models.post import Post
 from models.comment import Comment
 from models.user import User
+from resources.user import UsersDetail, AllUsers
 from resources.comment import UserComments, PostComments
 from resources.auth import Register, Login
+from resources.post import Posts, UserPosts
 
 app = Flask(__name__)
 CORS(app)
@@ -20,9 +22,17 @@ app.config['SQLALCHEMY_ECHO'] = True
 db.init_app(app)
 migrate = Migrate(app, db)
 
-#auth
+# auth
 api.add_resource(Login, '/user/login')
 api.add_resource(Register, '/user/register')
+# User Routes
+api.add_resource(UsersDetail, '/user')
+api.add_resource(UsersDetail, '/user/<int:user_id>')
+api.add_resource(AllUsers, '/users')
+# Post Routes
+api.add_resource(UserPosts, '/user/posts')
+api.add_resource(Posts, '/posts')
+api.add_resource(Posts, '/posts/<int:post_id>')
 # Comment Routes
 api.add_resource(PostComments, '/posts/comments/<int:post_id>')
 api.add_resource(UserComments, '/user/comments')
