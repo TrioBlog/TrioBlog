afdc17f3d08a883ac149207796c447def9f7b44a
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-07 17:32:22 -0500

    merg

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from models.db import db
 from models.post import Post
 from models.comment import Comment
 from models.user import User
-from resources import auth, user, comment, post
+from resources.comment import UserComments, PostComments
+from resources.auth import Register, Login
 
 app = Flask(__name__)
 CORS(app)
@@ -19,22 +20,14 @@ app.config['SQLALCHEMY_ECHO'] = True
 db.init_app(app)
 migrate = Migrate(app, db)
 
-# auth
-api.add_resource(auth.Login, '/user/login')
-api.add_resource(auth.Register, '/user/register')
-# User Routes
-api.add_resource(user.UsersDetail, '/user')
-api.add_resource(user.UsersDetail, '/user/<int:user_id>')
-api.add_resource(user.AllUsers, '/users')
-# Post Routes
-api.add_resource(post.UserPosts, '/user/posts')
-api.add_resource(post.Posts, '/posts')
-api.add_resource(post.Posts, '/posts/<int:post_id>')
+#auth
+api.add_resource(Login, '/user/login')
+api.add_resource(Register, '/user/register')
 # Comment Routes
-api.add_resource(comment.PostComments, '/posts/comments/<int:post_id>')
-api.add_resource(comment.UserComments, '/user/comments')
-api.add_resource(comment.UserComments, '/user/comment/<int:comment_id>')
-api.add_resource(comment.UserComments, '/user/comments/<int:user_id>')
+api.add_resource(PostComments, '/posts/comments/<int:post_id>')
+api.add_resource(UserComments, '/user/comments')
+api.add_resource(UserComments, '/user/comment/<int:comment_id>')
+api.add_resource(UserComments, '/user/comments/<int:user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
