599c9259bcc0d1f967634d322288f739d2c78118
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-07 11:30:17 -0500

    added user model

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models.db import db
+from models.user import User
 
 app = Flask(__name__)
 CORS(app)
---
+++ models/user.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from models.db import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class User(db.Model):
+  __tablename__ = 'users'
+
+  id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+  user_name = db.Column(db.String(255), nullable=False, unique=True)
+  password_digest = db.Column(db.String(255), nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
+  updated_at = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False, onupdate=datetime.now())
+
+#Associations
+  post = db.relationship("Post", backref=db.backref('posts', lazy=True))
+  comment = db.relationship("Comment", backref=db.backref('comments', lazy=True))
+
+  def __init__(self, user_name, password_digest):
+    self.user_name = user_name
+    self.password_digest = password_digest
+
+  def json(self):
+    return {
+      "id": self.id,
+      "user_name": self.user_name,
+      "password_digest": self.password_digest,
+      "created_at": str(self.created_at),
+      "updated_at": str(self.updated_at)
+    }
+
+  def create(self):
+    db.session.add(self)
+    db.session.commit()
+    return self
+
+  @classmethod
+  def find_all(cls):
+    return User.query.all()
+
+  @classmethod
+  def find_by_id(cls, id):
+    user = User.query.filter_by(id=id).first()
+    return user
