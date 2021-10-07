fc173615a8125861106da701ebf8d85aff937b89
Sam'an <samangriffiths47@gmail.com>
2021-10-07 14:48:31 -0500

    :open_book: comment resources init

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models.db import db
-from models.user import User
-
+from models import user, post, comment
+from resources import comment, user, post
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
@@ -16,8 +16,11 @@ app.config['SQLALCHEMY_ECHO'] = True
 db.init_app(app)
 migrate = Migrate(app, db)
 
-#routes
-
+# Comment Routes
+api.add_resource(comment.PostComments, 'posts/comments/<int:post_id>')
+api.add_resource(comment.UserComments, 'user/comments')
+api.add_resource(comment.UserComments, 'user/comment/<int:comment_id>')
+api.add_resource(comment.UserComments, 'user/comments/<int:user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
