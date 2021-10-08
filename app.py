b2c266616b0c62db626a58c882f470ad9bd8d030
Sam'an <samangriffiths47@gmail.com>
2021-10-07 19:12:20 -0500

    :world_map: route organization

--- app.py
+++ app.py
@@ -3,13 +3,7 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models.db import db
-from models.post import Post
-from models.comment import Comment
-from models.user import User
-from resources.user import UsersDetail, AllUsers
-from resources.comment import UserComments, PostComments
-from resources.auth import Register, Login
-from resources.post import Posts, UserPosts
+from resources import user, auth, comment, post
 
 app = Flask(__name__)
 CORS(app)
@@ -23,17 +17,20 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 # auth
-api.add_resource(Login, '/user/login')
-api.add_resource(Register, '/user/register')
+api.add_resource(auth.Login, '/user/login')
+api.add_resource(auth.Register, '/user/register')
 # User Routes
-api.add_resource(UsersDetail, '/user/<string:user_id>')
-api.add_resource(AllUsers, '/users')
+api.add_resource(user.UsersDetail, '/user/<string:user_id>')
+api.add_resource(user.AllUsers, '/users')
 # Post Routes
-api.add_resource(UserPosts, '/user/posts')
-api.add_resource(Posts, '/posts/<int:post_id>')
+api.add_resource(post.UserPosts, '/user/<string:<user_id>')
+api.add_resource(post.PostId, '/posts/<string:post_id>')
+api.add_resource(post.Post, '/posts')
 # Comment Routes
-api.add_resource(PostComments, '/posts/comments/<int:post_id>')
-api.add_resource(UserComments, '/user/comments/<int:user_id>')
+api.add_resource(comment.PostComments, '/posts/comments/<string:post_id>')
+api.add_resource(comment.UserComments, '/user/comments/<string:user_id>')
+api.add_resource(comment.Comments, '/comments')
+api.add_resource(comment.CommentId, '/comments/<string:comment_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
