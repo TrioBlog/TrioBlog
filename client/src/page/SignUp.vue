294564de7033751326b35ec4860602bdc14bf45e
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-08 14:28:42 -0500

    sign in and register styled

--- client/src/page/SignUp.vue
+++ client/src/page/SignUp.vue
@@ -1,17 +1,19 @@
 <template>
-  <div >
-    <h1>Sign Up</h1>
-    <form @submit.prevent="onSubmit">
-    <div>
-      <h2>Username:</h2>
-        <input type="username" :value="username" @input="handleChange" />
+  <div class="flexCon">
+    <div class="divCon">
+      <h1>Sign Up</h1>
+      <form @submit.prevent="onSubmit">
+      <div>
+        <h2>Username:</h2>
+          <input type="username" :value="username" @input="handleChange" />
+      </div>
+      <div>
+        <h2>Password:</h2>
+          <input type="password" :value="password" @input="handlePass" />
+      </div>
+      <button type="submit" :disabled="username ==='' || password === ''">SignUp</button>
+      </form>
     </div>
-    <div>
-      <h2>Password:</h2>
-        <input type="password" :value="password" @input="handlePass" />
-    </div>
-    <button type="submit" :disabled="username ==='' || password === ''">SignUp</button>
-    </form>
   </div>
 </template>
 
@@ -49,3 +51,50 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+input {
+  background-color: #CAF0F8;
+  font-size: 24px;
+  width: 175px;
+  border-radius: 10px;
+  border: 3px solid #03055e9b;
+}
+button {
+  margin-top: 2vh;
+  background-color: #CAF0F8;
+  color: #03045E;
+  border-radius: 2vh;
+  border: 2px solid #03055ecf;
+  width: 100px;
+  height: 40px;
+  font-size: 20px;
+}
+a {
+  text-decoration: none;
+  color: #03045E;
+  font-size: 18px;
+  background-color: #CAF0F8;
+  padding: 6px;
+  border-radius: 10px;
+  border: 2px solid #03045E;
+}
+h1,h2 {
+  margin: 1vh;
+}
+.flexCon {
+  display: flex;
+  justify-content: center;
+}
+.divCon {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  color: #03045E;
+  background-color: #90E0EF;
+  width: 30vh;
+  height: 40vh;
+  border-radius: 5vh;
+}
+</style>
