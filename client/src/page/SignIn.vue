cc16fbfa637e8d448e22da473fe299842ab1247d
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-10-08 14:09:06 -0500

    styling

--- client/src/page/SignIn.vue
+++ client/src/page/SignIn.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="divCon">
-    <h1>Sign In</h1>
-    <form @submit.prevent="onSubmit" >
-      <div>
-        <h2>Username:</h2>
-        <input type="username" :value="username" @input="handleChange"  />
-      </div>
-      <div>
-        <h2>Password:</h2>
-        <input type="password" :value="password" @input="handlePass" />
-      </div>
-      <div>
-        <a href="/">New Account?</a>
-      </div>
-      <button type="submit" :disabled="username ==='' || password === ''">Sign In</button>
-    </form>
+  <div class="flexCon">
+    <div class="divCon">
+      <h1>Sign In</h1>
+      <form @submit.prevent="onSubmit" >
+        <div>
+          <h2>Username:</h2>
+          <input type="username" :value="username" @input="handleChange"  />
+        </div>
+        <div>
+          <h2>Password:</h2>
+          <input type="password" :value="password" @input="handlePass" />
+        </div>
+        <div class="linkdiv">
+          <a href="/">New Account?</a>
+        </div>
+        <button type="submit" :disabled="username ==='' || password === ''">Sign In</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -41,7 +43,7 @@ export default {
       if (res.data){
         localStorage.setItem('token', res.data)
         this.password = ''
-        this.$router.push({ path: `/home`, props: this.username })
+        this.$router.push({ path: `/home`, props: {"username": this.username }})
       } else {
         alert("unauthorized")
       }
@@ -55,10 +57,42 @@ export default {
 
 <style scoped>
 input {
-  background-color: aqua;
-  padding: 10px;
+  background-color: #CAF0F8;
+  font-size: 24px;
+  width: 175px;
+  border-radius: 10px;
+  border: 3px solid #03055e9b;
+}
+button {
+  background-color: #CAF0F8;
+  color: #03045E;
+  border-radius: 2vh;
+  border: 2px solid #03055ecf;
+  width: 15vh;
+  height: 7vh;
+  font-size: 36px;
+}
+h1,h2 {
+  margin: 2vh;
+}
+.flexCon {
+  display: flex;
+  justify-content: center;
+}
+.linkdiv {
+  margin: 3vh;
+  text-decoration: none;
+  color: #03045E;
 }
 .divCon {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   text-align: center;
+  color: #03045E;
+  background-color: #90E0EF;
+  width: 50vh;
+  height: 75vh;
+  border-radius: 5vh;
 }
 </style>
